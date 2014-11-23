4bceb7eb538ba3d062db39a79521c7294b07e730
ratish123 <ratish.mr@gmail.com>
2014-11-23 10:05:30 +0530

    testmaven
    testmaven

---
+++ src/main/java/hello/HelloMaven.java
@@ -0,0 +1,8 @@
+package main.java.hello;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
